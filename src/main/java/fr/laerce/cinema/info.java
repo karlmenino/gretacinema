f05545f5c1c646aaff604f71beea41b574b0cd77
karlmenino <karl.menino@gmail.com>
2018-11-07 15:31:54 +0100

    suite de l'exercice

--- src/main/java/fr/laerce/cinema/info.java
+++ src/main/java/fr/laerce/cinema/info.java
@@ -19,14 +19,17 @@ public class info extends HttpServlet {
             FilmsDonnees fd = new FilmsDonnees();
             Film film = fd.getById(id);
             PrintWriter out = response.getWriter();
+            ServletContext cntx= getServletContext();
+            String filename = cntx.getRealPath("WEB-INF/datas/affiches/"+film.afficheNom);
                 out.println("<!DOCTYPE html>");
                 out.println("<html>");
                 out.println("<head>");
-                out.println("<title>info de "+film.afficheNom+"</title>");
+                out.println("<title>info de "+film.titre+"</title>");
                 out.println("</head>");
                 out.println("<body>");
-                out.println("<h1>"+film.afficheNom+"<h1>");
-                out.println("<ul>");
+                out.println("<h1>"+film.titre+"<h1>");
+                out.println("<img height='150' width='150' src='/affiche?id="+id+"'></img>");
+                out.println("<h1>la note de ce film est "+film.note+"<h1>");
                 out.println("</html>");
 
         }
